aa05d03b9b1e68e6aefe8b8c49d3e32fccb99686
Minjae Jeong <165645186+mjayJeong@users.noreply.github.com>
2025-03-11 20:02:19 +0900

    Create 멀리뛰기.c

---
+++ Programmers/멀리뛰기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int n) {
+    long long answer = 0;
+    int dp[2001];
+    
+    dp[1] = 1, dp[2] = 2;
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    answer = dp[n];
+    return answer;
+}
