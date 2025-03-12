b725b2e60357465a114bad8713559285f58410e5
Minjae Jeong <165645186+mjayJeong@users.noreply.github.com>
2025-03-12 11:28:10 +0900

    Create 기능개발.cpp

---
+++ Programmers/기능개발.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> days;
+
+    for (int i = 0; i < progresses.size(); i++) {
+        int remaining = 100 - progresses[i];
+        int required_days = (remaining + speeds[i] - 1) / speeds[i];
+        days.push(required_days);
+    }
+
+    while (!days.empty()) {
+        int count = 1; 
+        int first = days.front();
+        days.pop();
+
+        while (!days.empty() && days.front() <= first) {
+            count++;
+            days.pop();
+        }
+
+        answer.push_back(count);
+    }
+
+    return answer;
+}
