629a350fd69e74ea84eb555a722531994e405ace
Minjae Jeong <wjdalswo0312@naver.com>
2025-05-14 11:08:34 +0900

    Create 전력망을 둘로 나누기.java

---
+++ KB boot/week4/전력망을 둘로 나누기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] wires) {
+        int answer = n;
+
+        for (int i = 0; i < wires.length; i++) {
+            List<List<Integer>> graph = new ArrayList<>();
+            for (int j = 0; j <= n; j++) graph.add(new ArrayList<>());
+
+            for (int j = 0; j < wires.length; j++) {
+                if (i == j) continue;
+                int a = wires[j][0];
+                int b = wires[j][1];
+                graph.get(a).add(b);
+                graph.get(b).add(a);
+            }
+
+            boolean[] visited = new boolean[n + 1];
+            int count = bfs(1, graph, visited);
+            int diff = Math.abs((n - count) - count);
+            answer = Math.min(answer, diff);
+        }
+
+        return answer;
+    }
+
+    private int bfs(int start, List<List<Integer>> graph, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int next : graph.get(curr)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
