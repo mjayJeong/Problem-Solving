a1faca3fbc8f7a42383c40345b48078f7da1cb96
Minjae Jeong <wjdalswo0312@naver.com>
2026-01-02 19:32:15 +0900

    Create 2631.cpp

---
+++ Baekjoon/2631.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> dp(N, 1);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    int lis = *max_element(dp.begin(), dp.end());
+    cout << N - lis << '\n'; 
+
+    return 0;
+}
