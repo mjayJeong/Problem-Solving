5d4c09d8938c6ad823de454b84e32f303d2019df
Minjae Jeong <wjdalswo0312@naver.com>
2026-01-14 19:22:07 +0900

    Create 11004.cpp

---
+++ Baekjoon/11004.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+    vector<int> v(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+
+    cout << v[K-1] << '\n';
+
+    return 0;
+}
