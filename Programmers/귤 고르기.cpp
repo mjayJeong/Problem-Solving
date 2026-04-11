18bcf3047f4f1e9571c03c1ec83c150b1aff4d3c
Minjae Jeong <wjdalswo0312@naver.com>
2026-04-11 22:49:48 +0900

    Create 귤 고르기.cpp

---
+++ Programmers/귤 고르기.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, vector<int> tangerine) {
+    int answer = 0;
+    vector<int> cnt(100000001, 0);
+    int max_value = 0;
+    // 카운팅 배열에 각 숫자 개수 저장
+    for (int i = 0; i < tangerine.size(); i++) {
+        cnt[tangerine[i]]++;
+        max_value = max(max_value, tangerine[i]);
+    }
+    
+    // pq에 개수 많은 순으로 저장
+    priority_queue<pair<int, int>> pq;
+    for (int i = 1; i <= max_value; i++) {
+        pq.push({cnt[i], i});
+    }
+    
+    int now = 0;
+    while (now < k) {
+        auto [num, id] = pq.top();
+        pq.pop();
+        
+        now += num;
+        answer++;
+    }
+    
+    
+    return answer;
+}
